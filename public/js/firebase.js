9fc98d0c5b40751e95c64fc7ac176774f8a32972
javalillo13689 <james.valencia@pucp.edu.pe>
2019-11-16 04:33:42 -0500

    iniciando app-firebase

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDZCbXxIP0gWHjBFJOEAZ2_k010T11Plkc",
+  authDomain: "tobhs-b018f.firebaseapp.com",
+  databaseURL: "https://tobhs-b018f.firebaseio.com",
+  projectId: "tobhs-b018f",
+  storageBucket: "tobhs-b018f.appspot.com",
+  messagingSenderId: "803328934781",
+  appId: "1:803328934781:web:0c2b0559916d1c03c08214"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
