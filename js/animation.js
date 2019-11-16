8a7c245f094e432ab756ef61afc44c074b76c795
florball <flor_ballinas@hotmail.com>
2019-11-16 02:53:31 -0500

    first commit

---
+++ js/animation.js
@@ -0,0 +1,15 @@
+// Scroll Reveal
+window.sr = ScrollReveal();
+
+// Animación a logo, call to action, button
+sr.reveal('.logo, .section-home__txt, .section-home__btn', {
+  distance: '200px',
+  duration: 1000,
+  origin: 'left',
+});
+// Animación a section-home
+sr.reveal('.section-home__isotipo, .section-home__title, .section-home__subtitle', {
+  distance: '200px',
+  duration: 1000,
+  origin: 'right',
+});
---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDxoqDygE65a668unM2qYF8O8SsTxHF-lM",
+    authDomain: "talent-on-board.firebaseapp.com",
+    databaseURL: "https://talent-on-board.firebaseio.com",
+    projectId: "talent-on-board",
+    storageBucket: "talent-on-board.appspot.com",
+    messagingSenderId: "822380765539",
+    appId: "1:822380765539:web:be69c89182a07a1941f356",
+    measurementId: "G-555SGJN4JP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
