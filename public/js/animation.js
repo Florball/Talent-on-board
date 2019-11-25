0a7fdc4514a9ef7af8d7eceac32956ee69ac7b9f
Flor Ballinas <flor_ballinas@hotmail.com>
2019-11-25 17:33:00 -0500

    añadiendo funcion filtrar

--- public/js/animation.js
+++ public/js/animation.js
@@ -4,12 +4,12 @@ window.sr = ScrollReveal();
 // Animación a logo, call to action, button
 sr.reveal('.logo, .section-home__txt, .section-home__btn', {
   distance: '200px',
-  duration: 1000,
-  origin: 'left',
+  duration: 1300,
+  origin: 'left'
 });
 // Animación a section-home
 sr.reveal('.section-home__isotipo, .section-home__title, .section-home__subtitle', {
   distance: '200px',
   duration: 1000,
-  origin: 'right',
+  origin: 'right'
 });
--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,37 +1,46 @@
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-  apiKey: 'AIzaSyCtFvs4lJ8mht9jxkvGXnnf-YffETHd2Q0',
-  authDomain: 'tobhs-1879e.firebaseapp.com',
-  projectId: 'tobhs-1879e'
+  apiKey: "AIzaSyCtFvs4lJ8mht9jxkvGXnnf-YffETHd2Q0",
+  authDomain: "tobhs-1879e.firebaseapp.com",
+  projectId: "tobhs-1879e"
 });
 
-var db = firebase.firestore();
+const db = firebase.firestore();
 
-//Agregar documentos
-function guardar() {
-  db.collection("users")({
-    // name: nombre,
-    // area: area
-  })
-}
+const searcher = document.getElementById("searcher");
+const btnSearch = document.getElementById("btn-search");
+const card = document.getElementById("hackspacers");
 
-//leer datos
-var card = document.getElementById('hackspacers');
-
-db.collection("users").onSnapshot((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    card.innerHTML +=
-      `
-      <div class="card mx-2 my-2" style="width: 18rem;">
-      <!--  <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-          <h5 class="card-title"></h5>
-          <h6 class="card-subtitle mb-2 text-muted">${doc.data().name}</h6>
-          <p class="card-text">${doc.data().area}</p>
-          <a href="${doc.data().red_social_1}" target="_blank" class="card-link"><i class="fab fa-linkedin-in"></i></a>
-          <a href="${doc.data().red_social_2}" target="_blank" class="card-link"><i class="fab fa-github"></i></a>
+//lee los datos y los pinta
+const filter = () => {
+  db.collection("users").onSnapshot(querySnapshot => {
+    card.innerHTML = '';
+  querySnapshot.forEach(doc => {
+    const txtSearch = searcher.value.toUpperCase();
+    const track = doc.data().area.toUpperCase();
+    if (track.indexOf(txtSearch) !== -1) {
+      card.innerHTML += `
+    <div class="card section-talent__card mx-2 my-2" style="width: 16rem;">
+      <img src="${doc.data().foto}" class="card-img-top section-talent__img" alt="Foto de perfil">
+      <div class="card-body section-talent__cont-icons">
+        <h5 class="card-title text-center">${doc.data().name}</h5>
+        <h6 class="card-subtitle mb-3 text-center">${doc.data().area}</h6>
+        <h6 class="card-text my-2">Experiencia: ${doc.data().experiencia}.</h6>
+        <h6 class="card-text mb-4">Último proyecto: <a href="${doc.data().proyecto}" target="_blank">Click aquí <i class="fas fa-mouse-pointer"></i></a></h6>
+        <div class="text-center">
+          <div class="section-talent__icons mx-2"><a href="${doc.data().red_social_1}" target="_blank" class="section-talent__link"><i class="fab fa-linkedin-in"></i></a></div>
+          <div class="section-talent__icons mx-2"><a href="${doc.data().red_social_2}" target="_blank" class="section-talent__link"><i class="fab fa-github"></i></a></div>
+          <div class="section-talent__icons mx-2"><a href="mailto:${doc.data().mail}" target="_blank" class="section-talent__link"><i class="fas fa-envelope"></i></a></div>
+          <div class="section-talent__icons mx-2"><a href="https://api.whatsapp.com/send?phone=51${doc.data().number}&text=Hola,%20te%20vi%20en%20la%20página%20de%20Talent%20On%20Board%20y%20me%20gustaría%20hablar%20contigo.&source=&data=" target="_blank" class="section-talent__link"><i class="fab fa-whatsapp"></i></a></div>
         </div>
       </div>
-      `
+    </div>
+    `;
+    } else {
+      console.log(doc.data().area)
+    }
   });
 });
+}
+searcher.addEventListener("keyup", filter);
+filter();
